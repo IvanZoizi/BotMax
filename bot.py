d156fd02d827290b52548187d95799893e040450
zooii <zooiiz1@yandex.ru>
2025-11-11 01:50:51 +0400

    Сделал ветку с регистрацией пользователя

--- bot.py
+++ bot.py
@@ -1,20 +1,61 @@
 import asyncio
+
+from aiogram.fsm.context import FSMContext
+from maxapi.context import MemoryContext
 from maxapi import Bot, Dispatcher, Router, F
 from maxapi.context import StatesGroup, State
-from maxapi.types import Message
+from maxapi.filters.command import Command
+from maxapi.types import Message, MessageCreated, BotStarted
 from config import token
-from utils import Dbase
+from utils import dbase, RegistrationStates
+from handlers import user_router
 
-router = Router()
+bot = Bot(token=token)
+dp = Dispatcher()
+
+
+@dp.bot_started()
+async def bot_started(event: BotStarted, context: MemoryContext):
+    print(event.user.user_id)
+    if dbase.get_user(event.from_user.user_id):
+        pass
+    else:
+        await event.message.answer("""
+🤖 Добро пожаловать в бот продуктивности "Фокус"!
+
+Здесь вы сможете:
+• Ставить цели и задачи
+• Использовать технику Pomodoro
+• Отслеживать привычки
+• Получать персональные рекомендации
+
+Для начала работы необходимо зарегистрироваться!
+Введите ваше имя!""")
+        await context.set_state(RegistrationStates.waiting_for_name)
+
+@dp.message_created(Command('start'))
+async def hello(event: MessageCreated, context: MemoryContext):
+    if dbase.get_user(event.from_user.user_id):
+        pass
+    else:
+        await event.message.answer("""
+🤖 Добро пожаловать в бот продуктивности "Фокус"!
+
+Здесь вы сможете:
+• Ставить цели и задачи
+• Использовать технику Pomodoro
+• Отслеживать привычки
+• Получать персональные рекомендации
+
+Для начала работы необходимо зарегистрироваться!
+Введите ваше имя!""")
+        await context.set_state(RegistrationStates.waiting_for_name)
+
+
 
 async def main():
     """Основная функция запуска бота"""
-    # Замените на ваш токен
-    dbase = Dbase("./db.sqlite")
-    bot = Bot(token=token)
-    dp = Dispatcher()
-    # Подключаем роутер
-    dp.include_router(router)
+    dp.include_routers(user_router)
 
     print("Бот запущен...")
     await dp.start_polling(bot)
@@ -22,3 +63,36 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+
+# import asyncio
+# import logging
+#
+# from maxapi import Bot, Dispatcher
+# from maxapi.types import BotStarted, Command, MessageCreated
+#
+# logging.basicConfig(level=logging.INFO)
+#
+# bot = Bot('f9LHodD0cOL8I42VUpR9-7WcCKp1WvtyPFrSz1PvS1jRNRdlkYTRpIb2vby-HfRqWbkuT1UF-3MJFhYFuR1g')
+# dp = Dispatcher()
+#
+# # Ответ бота при нажатии на кнопку "Начать"
+# @dp.bot_started()
+# async def bot_started(event: BotStarted):
+#     await event.bot.send_message(
+#         chat_id=event.chat_id,
+#         text='Привет! Отправь мне /start'
+#     )
+#
+# # Ответ бота на команду /start
+# @dp.message_created(Command('start'))
+# async def hello(event: MessageCreated):
+#     await event.message.answer(f"Пример чат-бота для MAX 💙")
+#
+#
+# async def main():
+#     await dp.start_polling(bot)
+#
+#
+# if __name__ == '__main__':
+#     asyncio.run(main())
--- utils/dbase.py
+++ utils/dbase.py
@@ -7,3 +7,7 @@ class Dbase:
 
     def get_user(self, user_id):
         return self.cursor.execute("""SELECT * FROM users WHERE user_id = ?""", (user_id,)).fetchone()
+
+    def new_user(self, user_id, name, email, goal, steps):
+        self.cursor.execute("""INSERT INTO users (user_id, name, email, goal, steps) VALUES (?, ?, ?, ?, ?)""", (user_id, name, email, goal, steps))
+        self.conn.commit()
