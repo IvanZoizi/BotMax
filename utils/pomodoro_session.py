91d013fc036b3d90455933514fc3782c4ebc8e62
Alex <goldmaster751@gmail.com>
2025-11-15 10:18:24 +0400

    fix time

--- utils/pomodoro_session.py
+++ utils/pomodoro_session.py
@@ -7,9 +7,9 @@ class PomodoroSession:
     def __init__(self, event_id: int, user_id: int):
         self.event_id = event_id
         self.user_id = user_id
-        self.work_duration = 25#25 * 60
-        self.break_duration = 5 #* 60
-        self.long_break_duration = 15 #* 60
+        self.work_duration = 25 * 60
+        self.break_duration = 5 * 60
+        self.long_break_duration = 15 * 60
         self.pomodoros_completed = 0
         self.is_working = False
         self.is_break = False
